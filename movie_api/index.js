aa101d31cbe16f353136947907babb9f2dec7e65
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-05-24 07:38:57 +0200

    Update index.js
    Adding movies, error-handling middleware,  morgan middleware and express routing to index.js

--- movie_api/index.js
+++ movie_api/index.js
@@ -1,21 +1,83 @@
-    // required to import express into package
 const express = require('express'),
-    app = express();
-    // http module
-const http = require('http'),
-    url = require('url');
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path');
 
-http.createServer((request, response) => {
-    let requestURL = url.parse(request.url, true);
-    if (requestURL.pathname == '/documentation.html') {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('Documentation on the book club?\n');
-    }else{
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('Welcome to my book club.\n');
-    }
+const app = express();
 
-}).listen(8080);
+    // Create a write stream (in appen mode)
+    // A "log.txt" file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
-console.log('My Node test server.');
+    let movies = [
+        {
+            title: 'Predator',
+            director: 'John McTiernan'
+        },
+        {
+            title: 'Commando',
+            director: 'Mark L. Lester'
+        },
+        {
+            title: 'Lethal Weapon',
+            director: 'Richard Donner'
+        },
+        {
+            title: 'Die Hard',
+            director: 'John McTiernan'
+        },
+        {
+            title: 'Ghostbusters',
+            director: 'Ivan Reitman'
+        },
+        {
+            title: 'The Burbs',
+            director: 'Joe Dante'
+        },
+        {
+            title: 'Big',
+            director: 'Penny Marshall'
+        },
+        {
+            title: 'Flight of the Navigator',
+            director: 'Randal Kleiser'
+        },
+        {
+            title: 'Alien',
+            director: 'Ridley Scott'
+        },
+        {
+            title: 'Aliens',
+            director: 'James Cameron'
+        },
+        {
+            title: 'The Abyss',
+            director: 'James Cameron'
+        },
+    ];
 
+    // Get requests
+    app.get('/', (req, res) => {
+        res.send('Swagflix coming soon!');
+    });
+
+    app.get('/movies', (req, res) => {
+        res.json(movies);
+    });
+    
+    // Setup the logger middleware
+    app.use(morgan('combined', {stream: accessLogStream}));
+
+    // Sends static files to log.txt
+    app.use(express.static('public'));
+
+    // Error handling
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    });
+
+    // Listen for requests
+    app.listen(8080, () => {
+        console.log("I'm running!");
+    });
