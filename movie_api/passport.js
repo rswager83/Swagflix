c37b947e384a21f3f376bb31557513a836349f2f
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-06-08 12:25:11 +0200

    Update passport.js

--- movie_api/passport.js
+++ movie_api/passport.js
@@ -1,46 +1,3 @@
-// const passport = require('passport'),
-//     LocalStrategy = require('passport-local').Strategy,
-//     Models = require('./models.js'),
-//     passportJWT = require('passport-jwt');
-
-// let Users = Models.User,
-//     JWTStrategy = passportJWT.Strategy,
-//     ExtractJWT = passportJWT.ExtractJwt;
-
-// passport.use(new LocalStrategy({
-//     usernameField: 'Username',
-//     passwordField: 'Password'
-// }, (username, password, callback) => {
-//     console.log(username + ' ' + password);
-//     Users.findOne({ Username: username }, (error, user) => {
-//         if (error) {
-//             console.log(error);
-//             return callback(error);
-//         }
-
-//         if (!user) {
-//             console.log('incorrect username');
-//             return callback(null, false, {message: 'Incorrect username or password.'});
-//         }
-
-//         console.log('finished');
-//         return callback(null, user);
-//     });
-// }));
-
-// passport.use(new JWTStrategy({
-//     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-//     secretOrKey: '123JWTSecretCode'
-// }, (jwtPayload, callback) => {
-//     return Users.findById(jwtPayload._id)
-//         .then((user) => {
-//             return callback(null, user);
-//         })
-//         .catch((error) => {
-//             return callback(error)
-//         });
-// }));
-
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
@@ -50,6 +7,7 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
+  // login endpoint
 passport.use(new LocalStrategy({
   usernameField: 'Username',
   passwordField: 'Password'
@@ -73,7 +31,7 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
+  secretOrKey: '123JwTsecretKeY'
 }, (jwtPayload, callback) => {
   return Users.findById(jwtPayload._id)
     .then((user) => {
